dbeb6d6937ac9e8f6dabc10b6f4289fffe232390
Ryan Ribeiro <ribeiro1110ryan@gmail.com>
2026-05-12 16:56:31 -0300

    Init: Saas de barbearia para APS

---
+++ src/main/java/com/rpteam/barberstore/BarberstoreApplication.java
@@ -0,0 +1,13 @@
+package com.rpteam.barberstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarberstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rpteam/barberstore/BarberstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpteam.barberstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
